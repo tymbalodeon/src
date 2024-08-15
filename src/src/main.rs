52bb0b9cedd5fe666a20d9e757c02fab40618cf2
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-08-15 14:06:28 -0400

    feat: init clap

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,7 +1,60 @@
-use lib;
+use clap::{Parser, Subcommand};
+
+/// Manage git repositories in an organized way
+#[derive(Parser)]
+#[command(arg_required_else_help(true))]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Cd
+    Cd,
+
+    /// Clone
+    Clone,
+
+    /// Ls
+    Ls,
+
+    /// New
+    New,
+
+    /// Remove
+    Remove,
+
+    /// Sync
+    Sync,
+}
 
 fn main() {
-    let result = lib::add(10, 42);
+    match &Cli::parse().command {
+        Some(Command::Cd {}) => {
+            println!("Implement me!");
+        }
 
-    println!("{result}")
+        Some(Command::Clone {}) => {
+            println!("Implement me!");
+        }
+
+        Some(Command::Ls {}) => {
+            println!("Implement me!");
+        }
+
+        Some(Command::New {}) => {
+            println!("Implement me!");
+        }
+
+        Some(Command::Remove {}) => {
+            println!("Implement me!");
+        }
+
+        Some(Command::Sync {}) => {
+            println!("Implement me!");
+        }
+
+        None => (),
+    }
 }
