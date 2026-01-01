0637c6ccf37ecc43aaaf2100e7e03b870f929c3f
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-01-01 09:08:10 -0500

    fix: config print with no newline

--- src/src/commands/config.rs
+++ src/src/commands/config.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use crate::config::get_config;
 
 pub fn config() -> Result<()> {
-    println!("{}", toml::to_string(&get_config()?)?);
+    print!("{}", toml::to_string(&get_config()?)?);
 
     Ok(())
 }
