921f2744a0378d6cb65a64deab0255bd08aeee81
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-04-25 09:25:58 -0400

    chore: update dependencies

--- nix/rust-overlay/flake-module.nix
+++ nix/rust-overlay/flake-module.nix
@@ -1,18 +1,20 @@
-{ inputs, ... }:
-let
+{inputs, ...}: let
   overlays = [
     (import inputs.rust-overlay)
-    (self: super: assert !(super ? rust-toolchain); rec {
-      rust-toolchain = super.rust-bin.fromRustupToolchainFile ../../rust-toolchain.toml;
+    (self: super:
+      assert !(super ? rust-toolchain); rec {
+        rust-toolchain = super.rust-bin.fromRustupToolchainFile ../../rust-toolchain.toml;
 
-      # buildRustCrate/crate2nix depend on this.
-      rustc = rust-toolchain;
-      cargo = rust-toolchain;
-    })
+        # buildRustCrate/crate2nix depend on this.
+        rustc = rust-toolchain;
+        cargo = rust-toolchain;
+      })
   ];
-in
-{
-  perSystem = { system, ... }: {
-    _module.args.pkgs = import inputs.nixpkgs { inherit system overlays; config = { }; };
+in {
+  perSystem = {system, ...}: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system overlays;
+      config = {};
+    };
   };
 }
